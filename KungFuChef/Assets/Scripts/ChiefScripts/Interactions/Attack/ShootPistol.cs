0e221ddb88022e91c70a5df5e68171628f0b70f8
SnowTelevision <pguan100@gmail.com>
2017-05-09 02:49:59 -0400

    Can shoot pistol. Pistol can shoot and destroy foods, pistol should be able to destroy drones(need test). Drones should spawn after the player get some score(need test). Drone can't zap player yet! No sound at all!

---
+++ KungFuChef/Assets/Scripts/ChiefScripts/Interactions/Attack/ShootPistol.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class ShootPistol : VRTK_InteractableObject
+{
+    public MultiShooter trigger;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    // Update is called once per frame
+    //void Update ()
+    //   {
+
+    //}
+
+    public override void StartUsing(GameObject usingObject)
+    {
+        trigger.shoot();
+
+
+    }
+}
